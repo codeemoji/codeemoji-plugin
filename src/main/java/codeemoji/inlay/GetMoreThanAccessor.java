9697a3ab01c02aa4d526257cc6388bcb3078fe8d
Fernando de Oliveira Pereira <fernando.pereira@cemaden.gov.br>
2023-07-25 10:14:35 -0300

    Refactoring.

---
+++ src/main/java/codeemoji/inlay/GetMoreThanAccessor.java
@@ -0,0 +1,59 @@
+package codeemoji.inlay;
+
+import codeemoji.core.CEMethodCollector;
+import codeemoji.core.CEProvider;
+import com.intellij.codeInsight.hints.InlayHintsCollector;
+import com.intellij.codeInsight.hints.InlayHintsSink;
+import com.intellij.codeInsight.hints.NoSettings;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+public class GetMoreThanAccessor extends CEProvider<NoSettings> {
+
+    @Override
+    public @Nullable String getPreviewText() {
+        return """
+                public class Customer {
+                    public List<Item> getItems() {
+                        if (items == null) {
+                            List<Item> list = new ArrayList<>();
+                            Sale sale = getSale();
+                            for (Item i : sale.items()) {
+                                if (i.confirmed()){
+                                    list.add(i);
+                                }
+                            }
+                            list = Collections.unmodifiableList(list);
+                        }
+                        return items;
+                    }
+                }""";
+    }
+
+    @Override
+    public InlayHintsCollector getCollector(@NotNull Editor editor, @NotNull String keyId) {
+        return new CEMethodCollector(editor, keyId) {
+            @Override
+            public void processInlayHint(@Nullable PsiMethod method, InlayHintsSink sink) {
+                if (method != null && method.getName().startsWith("get") &&
+                        !(Objects.equals(method.getReturnType(), PsiTypes.voidType())) &&
+                        Objects.requireNonNull(method.getBody()).getStatements().length > 1) {
+                    addInlayHint(method, sink, 0x1F937, true);
+                }
+            }
+        };
+    }
+}
+
+
+
+
+
+
+
+
