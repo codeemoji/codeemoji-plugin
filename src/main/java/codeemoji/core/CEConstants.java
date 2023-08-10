97851a13c9e0bb668035a137d71d1284a64d5613
Fernando de Oliveira Pereira <fernando.pereira@cemaden.gov.br>
2023-08-10 09:42:47 -0300

    Fix emojis.

--- src/main/java/codeemoji/core/CEConstants.java
+++ src/main/java/codeemoji/core/CEConstants.java
@@ -11,18 +11,18 @@ public class CEConstants {
     public static final CESymbol ONE = new CESymbol(0x261D);
 
     public static final CESymbol SYNCHRONIZED_SYMBOL = new CESymbol(0x1F6A6); //traffic light
-    public static final CESymbol PUBLIC_SYMBOL = new CESymbol(0x1F31F); //star
-    public static final CESymbol PRIVATE_SYMBOL = new CESymbol(0x1F910); //zipper-mouth
+    public static final CESymbol PUBLIC_SYMBOL = new CESymbol(0x1F30E); //globe
+    public static final CESymbol PRIVATE_SYMBOL = new CESymbol(0x1F510); //padlock with key
     public static final CESymbol PROTECTED_SYMBOL = new CESymbol(0x1F6E1); //shield
     public static final CESymbol ABSTRACT_SYMBOL = new CESymbol(0x1F3A8); //artist palette
     public static final CESymbol DEFAULT_INTERFACE_SYMBOL = new CESymbol(0x1F503); //arrow curving
-    public static final CESymbol DEFAULT_SYMBOL = new CESymbol(0x1F3E0); //house
-    public static final CESymbol NATIVE_SYMBOL = new CESymbol(0x2699); //gear
-    public static final CESymbol STRICTFP_SYMBOL = new CESymbol(0x1F4D0); //triangle ruler
+    public static final CESymbol DEFAULT_SYMBOL = new CESymbol(0x1F310); //globe with meridians
+    public static final CESymbol NATIVE_SYMBOL = new CESymbol(0x1F4BB); //laptop
+    public static final CESymbol STRICTFP_SYMBOL = new CESymbol(0x1F4CF); //straight ruler
     public static final CESymbol VOLATILE_SYMBOL = new CESymbol(0x26A1); //high voltage
-    public static final CESymbol TRANSIENT_SYMBOL = new CESymbol(0x2601); //cloud
+    public static final CESymbol TRANSIENT_SYMBOL = new CESymbol(0x23F3); //hourglass
     public static final CESymbol FINAL_SYMBOL = new CESymbol(0x1F512); //locked
-    public static final CESymbol STATIC_SYMBOL = new CESymbol(0x1F310); //globe
+    public static final CESymbol STATIC_SYMBOL = new CESymbol(0x2699); //gear
 
     public static final CESymbol PADLOCK = new CESymbol(new ImageIcon(Objects.requireNonNull(CEConstants.class.getResource("/icons/padlock.png"))));
 }
