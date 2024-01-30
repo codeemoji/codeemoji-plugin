4f067a866d9c24b3335e3e0e132b287c1a2c6ba6
mpenduzzu <mpenduzzu@unibz.it>
2024-01-30 08:57:11 +0100

    Added emojis to StructuralInspectionSymbols enum

--- src/main/java/codeemoji/inlay/structuralinspection/StructuralInspectionSymbols.java
+++ src/main/java/codeemoji/inlay/structuralinspection/StructuralInspectionSymbols.java
@@ -5,6 +5,9 @@ import codeemoji.core.util.CESymbol;
 public enum StructuralInspectionSymbols {
 
     ;
-    public static final CESymbol PURE_GETTER = new CESymbol(0x1F1EC);
-    public static final CESymbol PURE_SETTER = new CESymbol(0x1F1F8);
+    public static final CESymbol PURE_ACCESSOR = new CESymbol(0x1F1EC);
+    public static final CESymbol PURE_MUTATOR = new CESymbol(0x1F1F8);
+    public static final CESymbol STATE_INDEPENDENT_METHOD = new CESymbol(0x1F5FF);
+    public static final CESymbol STATE_CHANGING_METHOD = new CESymbol(0x1F3AD);
+    public static final CESymbol SIDE_EFFECTED_METHOD = new CESymbol(0x1F39B);
 }
